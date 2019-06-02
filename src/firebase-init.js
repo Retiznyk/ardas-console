4fa6e0f7d64956ccce45f11f43f06f7c5d9f5fcb
Igor Retiznyk <igor.retiznyk@ardas.biz>
2019-06-02 14:17:26 +0300

    init firebase

---
+++ src/firebase-init.js
@@ -0,0 +1,13 @@
+import firebase from '@firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGHJtKeVPE8RoKe87SuBHfntfJ2Eo11aU',
+  authDomain: 'ardas-console.firebaseapp.com',
+  databaseURL: 'https://ardas-console.firebaseio.com',
+  projectId: 'ardas-console',
+  storageBucket: 'ardas-console.appspot.com',
+  messagingSenderId: '47757987072',
+  appId: '1:47757987072:web:5e59624fcc6afc46',
+};
+
+firebase.initializeApp(firebaseConfig);
